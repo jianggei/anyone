834805ffd914253f72d7afdd918c003289d5764c
longjiang <1023709046@qq.com>
2019-11-29 17:44:23 +0800

    update

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const conf = require('./config/configServers')
+const chalk = require('chalk')
+const path = require('path')
+const router = require('./router/router')
+const server = http.createServer((req , res) => {
+    
+    const filePath = path.join(conf.root , req.url) //req.url 代表端口后面的路劲
+    router(req, res , filePath)
+
+})
+
+server.listen( conf.port , conf.hostname , (err) => {
+    let d = `http://${conf.hostname}:${conf.port}`
+    console.log(`the server is run ${chalk.green(d)}`)
+})
---
+++ src/router/router.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const promisify = require('util').promisify
+const handlebars = require('handlebars')
+
+
+const stat = promisify(fs.stat)
+const conf = require('../config/configServers')
+const data = fs.readFileSync(__dirname+'/../template/dir.tpl')
+const readdir = promisify(fs.readdir)
+const template = handlebars.compile(data.toString());
+module.exports = async (req , res , filePath) => {
+    res.statusCode = 200
+    res.setHeader('Content-type' , 'text/html' );
+    try{
+        const file = await stat(filePath);
+        if(file.isFile()){
+          
+            fs.createReadStream(filePath).pipe(res)
+
+        }else if(file.isDirectory()){
+
+            const files = await readdir(filePath)
+            const relatievPath = path.relative( conf.root , filePath )
+            const data = {
+                dir:relatievPath ? relatievPath : '',
+                files
+            }
+
+            res.end(template(data))
+        }
+
+    }
+    catch(err){
+        res.statusCode = 404
+        res.setHeader('Content-type' , 'text/html');
+    
+        res.end(`no such file or directory\n ${err}`);
+        return;
+
+    }
+}
